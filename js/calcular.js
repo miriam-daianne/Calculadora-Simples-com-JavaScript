0b990b90e3fbaddb98a9de929738067cda1ddf2c
miriam-daianne <miriamdaianne@protonmail.com>
2024-07-30 18:58:03 -0300

    atualização dia 30/07

--- js/calcular.js
+++ js/calcular.js
@@ -6,38 +6,40 @@ let opAtual = null;
 let numAtual = null;
 
 
-function inserirNum(num){
+function inserirNum(num) {
     if (novoNum) {
         visor.textContent = num;
-        // visorMemoria.textContent += num;
         novoNum = false;
     } else {
         visor.textContent += num;
-        // visorMemoria.textContent += num;
     }
-}
-    
+
+    visorMemoria.textContent += num;
+
     numAtual = parseFloat(visor.textContent.replace(',', '.'));
-    
-    
+}
 
-    if(numAtual !== null && opAtual !== null){
-        numAnterior = numAtual;
-        numAtual = null;
-        novoNum = true;
-    }
-    
 
-function inserirOperador(op){
+
+
+if (numAtual !== null && opAtual !== null) {
+    numAnterior = numAtual;
+    numAtual = null;
+    novoNum = true;
+}
+
+
+
+function inserirOperador(op) {
     let ultimoDigito = visor.textContent.slice(-1);
-    
-    if(ultimoDigito == ","){
+
+    if (ultimoDigito == ",") {
         return;
     }
 
-    if(numAnterior !== null && numAtual !== null){
+    if (numAnterior !== null && numAtual !== null) {
         resultado();
-        numAtual = parseFloat(visor.textContent.replace(',','.'));
+        numAtual = parseFloat(visor.textContent.replace(',', '.'));
         numAnterior = null;
     }
 
@@ -47,7 +49,7 @@ function inserirOperador(op){
     novoNum = true;
 }
 
-function clean(){
+function clean() {
     visor.textContent = '';
     numAnterior = null;
     numAtual = null;
@@ -56,34 +58,40 @@ function clean(){
     visorMemoria.textContent = '';
 }
 
-function apagarDigito(){
+function apagarDigito() {
     visor.textContent = visor.textContent.slice(0, -1);
+    visorMemoria.textContent = visorMemoria.textContent.slice(0, -1);
 }
 
-function inverterSinal(){
+function inverterSinal() {
 
     let valor = parseFloat(visor.textContent.replace(',', '.'));
     if (!isNaN(valor)) {
         valor *= -1;
         visor.textContent = valor.toString().replace('.', ',');
+        visorMemoria.textContent = valor;
     }
 }
 
-function virgula(){
+function virgula() {
     if (novoNum) {
         visor.textContent += "0,";
         novoNum = false;
+        visorMemoria.textContent += "0,";
     } else if (!visor.textContent.includes(',')) {
         visor.textContent += ",";
+        visorMemoria.textContent += ",";
     }
+
+
 }
 
-function resultado (){
-    if(numAnterior !== null && numAtual !== null || opAtual !== null){
+function resultado(op) {
+    if (numAnterior !== null && numAtual !== null || opAtual !== null) {
 
         let resultado = 0;
-         
-        switch(opAtual){
+
+        switch (op) {
             case '+':
                 resultado = numAnterior + numAtual;
                 break;
@@ -91,7 +99,7 @@ function resultado (){
                 resultado = numAnterior - numAtual;
                 break;
             case '/':
-                if(numAtual === 0){
+                if (numAtual === 0) {
                     alert('Error: Divisão por zero é impossível!');
                     clean();
                     return;
@@ -103,20 +111,15 @@ function resultado (){
                 break;
         }
 
-        visor.textContent = resultado.toString().replace('.',',');
+        visor.textContent = resultado.toString().replace('.', ',');
         numAnterior = resultado;
-        numAtual =null;
+        numAtual = null;
         opAtual = null;
         novoNum = false;
     }
 }
 
 
+/* Problemas  */
 
-/* Problemas atuais  */
-
-/*
-- colocar os valores ja armazenados nas variáveis no visorMenor
-- arrumar função resultado
-
-*/
+/* arumar função resultado e verificar se os valores estão no parseflot certo */
